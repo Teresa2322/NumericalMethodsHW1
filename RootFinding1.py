c0006814acdab03a4f9a50281756cdafd58ddbfa
Teresa2322 <teresadelaguila23@gmail.com>
2026-01-13 19:18:29 -0500

    checkpoint

--- RootFinding1.py
+++ RootFinding1.py
@@ -14,18 +14,7 @@ from scipy.special import legendre
 #Also recall nth LP has n roots, need to find a way for the algorithm 
 #to scan the entire domain 
 
-'''
-trial = legendre(2)
-trialder = trial.deriv()
-
-print(trial)
-
-print(trialder)
-
-print("see if I can eval ", trial(-0.57735))
-'''
-
-tol = 10**(-14)
+tol = 10**(-15)
 
 
 def NRalgorithm(n):
@@ -40,19 +29,21 @@ def NRalgorithm(n):
 		x0 += 0.01
 		if func(x0)*func(x0 + 0.01) < 0: #using this to id a region with a root  
 			while abs(func(x0)) > tol:
-				x0 = x0 - (func(x0)/derf(x0))
-			 
-			RootArr.append(x0) #perhaps change this, its to find the next root
-		
-			print(x0)
+				x0 = x0 - (func(x0)/derf(x0))			 
+			RootArr.append(x0) 
 	return RootArr
 
-rootsP6 = NRalgorithm(6)
+#collecting sample points (in arrays, by degree of legendre polynomial)
+xp0 = NRalgorithm(0)
+xp1 = NRalgorithm(1)
+xp2 = NRalgorithm(2)
+xp3 = NRalgorithm(3)
+xp4 = NRalgorithm(4)
+xp5 = NRalgorithm(5)
+xp6 = NRalgorithm(6)
+xp7 = NRalgorithm(7)
 
-print("P0: ", NRalgorithm(0))
-print("P1: ", NRalgorithm(1))
-print("P2: ", NRalgorithm(2))
-print("P3: ", NRalgorithm(3))
+#Sanity check: checking number of roots:
+print("length checks:", len(xp0), len(xp1), len(xp2), len(xp3), len(xp4), len(xp5), len(xp6), len(xp7))
 
-#print("Evaluating P1", f1(-0.57735))
-#print("Second evaluation P1", f1(0.57735))
+
