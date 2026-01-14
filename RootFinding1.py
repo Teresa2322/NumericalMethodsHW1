6ae63937d23a4059206f6a311a35f344671ec552
Teresa2322 <teresadelaguila23@gmail.com>
2026-01-13 19:07:10 -0500

    update: successful root finding

--- RootFinding1.py
+++ RootFinding1.py
@@ -14,7 +14,7 @@ from scipy.special import legendre
 #Also recall nth LP has n roots, need to find a way for the algorithm 
 #to scan the entire domain 
 
-
+'''
 trial = legendre(2)
 trialder = trial.deriv()
 
@@ -23,22 +23,36 @@ print(trial)
 print(trialder)
 
 print("see if I can eval ", trial(-0.57735))
+'''
 
 tol = 10**(-14)
 
 
-def NRalgorithm( x, n):
-	
-	x0 = -1 
-	RootArr = []
-	while abs(f(x0)) > tol and x0 < 1:
-		x0 = x0 - (f(x0)/derf(x1))
-	else: 
-		RootArr.append(x0)
-		x0 = x0 + 0.00001 #perhaps change this, its to find the next root
+def NRalgorithm(n):
 
+	func = legendre(n)
+	derf = func.deriv()
+
+	x0 = -1 
+	RootArr = []	
+
+	while x0 < 1:
+		x0 += 0.01
+		if func(x0)*func(x0 + 0.01) < 0: #using this to id a region with a root  
+			while abs(func(x0)) > tol:
+				x0 = x0 - (func(x0)/derf(x0))
+			 
+			RootArr.append(x0) #perhaps change this, its to find the next root
+		
+			print(x0)
 	return RootArr
 
+rootsP6 = NRalgorithm(6)
+
+print("P0: ", NRalgorithm(0))
+print("P1: ", NRalgorithm(1))
+print("P2: ", NRalgorithm(2))
+print("P3: ", NRalgorithm(3))
 
 #print("Evaluating P1", f1(-0.57735))
 #print("Second evaluation P1", f1(0.57735))
